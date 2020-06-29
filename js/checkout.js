166e4e69ad35b62a2c1768a164688bccae988aad
Porkchopsss <pokoshih@gmail.com>
2020-06-29 18:36:08 +1000

    upload working checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -3,7 +3,7 @@ products = {
     "title": "cheeseburger",
     "yield": "1 serving",
     "description": "Grass-fed beef patty, cheese, mustard and tomato sauce in our signature bun.",
-    "img": "/images/menu0.png",
+    "img": "./images/menu0.png",
     "price": "10",
     "ingr": ["1 hamburger bun",
       "6 oz beef patty",
@@ -15,7 +15,7 @@ products = {
     "title": "LOT",
     "yield": "1 serving",
     "description": "Grass-fed beef patty, cheese, mustard, mayo, tomato sauce, tomato, lettuce and pickles in our signature bun.",
-    "img": "../images/menu1.png",
+    "img": "./images/menu1.png",
     "price": "12.9",
     "ingr": ["1 hamburger bun",
       "6 oz beef patty",
@@ -30,7 +30,7 @@ products = {
     "title": "FriedChickenBurger1",
     "yield": "1 serving",
     "description": "Grilled chicken breast or fried chicken thigh, tom-oli sauce and slaw in our signature bun.",
-    "img": "../images/menu2.png",
+    "img": "./images/menu2.png",
     "price": "14.5",
     "ingr": ["1 hamburger bun",
       "6 oz fried chicken",
@@ -43,7 +43,7 @@ products = {
     "title": "FriedChickenBurger2",
     "yield": "1 serving",
     "description": "Grilled chicken breast or fried chicken thigh, slaw, sriracha mayo and Handsome Devils Co hot sauce in our signature bun.",
-    "img": "../images/menu3.png",
+    "img": "./images/menu3.png",
     "price": "16",
     "ingr": ["1 hamburger bun",
       "6 oz fried chicken",
@@ -55,7 +55,7 @@ products = {
     "title": "VeganBurger",
     "yield": "1 serving",
     "description": "Spicy bean and quinoa patty, jalapeños, sriracha mayo, cheese, mustard, tomato sauce, tomato, lettuce and pickles in our signature bun.",
-    "img": "../images/menu4.png",
+    "img": "./images/menu4.png",
     "price": "15",
     "ingr": ["1 hamburger bun",
       "2 oz lettuce",
@@ -69,7 +69,7 @@ products = {
     "title": "chips",
     "yield": "1 serving",
     "description": "Choice of salt, chicken salt, or dill salt.",
-    "img": "../images/menu5.png",
+    "img": "./images/menu5.png",
     "price": "5.5",
     "ingr": [
       "5 oz potato"]
@@ -78,7 +78,7 @@ products = {
     "title": "coke 375mL",
     "yield": "1 serving",
     "description": "Choice of Coke, Coke No Sugar, Sprite or Fanta.",
-    "img": "../images/menu6.png",
+    "img": "./images/menu6.png",
     "price": "3.9",
     "ingr": [
       "1.42 oz sugar",
@@ -89,7 +89,7 @@ products = {
 
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("Cheeseburger")) > 0 ){
+  if (parseInt(localStorage.getItem("Cheeseburger")) > 0) {
     console.log("working")
     var data = products.cheeseburger;
     // the object of a cheeseburger
@@ -97,50 +97,47 @@ $(document).ready(function () {
     var burgerPrice = data.price;
     var imageBurger = products.cheeseburger.img;
     var quantity = localStorage.getItem("Cheeseburger");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"CheeseburgerPrice",
-            text: "Total: $" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "CheeseburgerPrice",
+            text: "Total: $" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
-
-    
-    
     $(".content-box").append(addImage);
-  }else{
+  } else {
     return
   }
-  
+
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("Lot Burger")) > 0 ){
+  if (parseInt(localStorage.getItem("Lot Burger")) > 0) {
     console.log("working")
     var data = products.LOTBurger;
     console.log(data);
@@ -150,45 +147,46 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.LOTBurger.img;
     var quantity = localStorage.getItem("Lot Burger");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"LotBurgerPrice",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "LotBurgerPrice",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("FriedChickenBurger1")) > 0 ){
+  if (parseInt(localStorage.getItem("FriedChickenBurger1")) > 0) {
     console.log("working")
     var data = products.FriedChickenBurger1;
     console.log(data);
@@ -198,45 +196,46 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.FriedChickenBurger1.img;
     var quantity = localStorage.getItem("FriedChickenBurger1");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"FriedChickenBurgerPrice1",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "FriedChickenBurgerPrice1",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("FriedChickenBurger2")) > 0 ){
+  if (parseInt(localStorage.getItem("FriedChickenBurger2")) > 0) {
     console.log("working")
     var data = products.FriedChickenBurger2;
     console.log(data);
@@ -246,45 +245,46 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.FriedChickenBurger2.img;
     var quantity = localStorage.getItem("FriedChickenBurger2");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"FriedChickenBurgerPrice2",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "FriedChickenBurgerPrice2",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("VeganBurger")) > 0 ){
+  if (parseInt(localStorage.getItem("VeganBurger")) > 0) {
     console.log("working")
     var data = products.VeganBurger;
     console.log(data);
@@ -294,45 +294,46 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.VeganBurger.img;
     var quantity = localStorage.getItem("VeganBurger");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"VeganBurgerPrice",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "VeganBurgerPrice",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("Chips")) > 0 ){
+  if (parseInt(localStorage.getItem("Chips")) > 0) {
     console.log("working")
     var data = products.chips;
     console.log(data);
@@ -342,45 +343,46 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.chips.img;
     var quantity = localStorage.getItem("Chips");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"ChipsPrice",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "ChipsPrice",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 $(document).ready(function () {
-  if(parseInt(localStorage.getItem("SoftDrink")) > 0 ){
+  if (parseInt(localStorage.getItem("SoftDrink")) > 0) {
     console.log("working")
     var data = products.Coke;
     console.log(data);
@@ -390,68 +392,90 @@ $(document).ready(function () {
     console.log(burgerPrice)
     var imageBurger = products.Coke.img;
     var quantity = localStorage.getItem("SoftDrink");
-    var addImage = $("<section>",{
-      class:"menuRow",
+    var addImage = $("<section>", {
+      class: "menuRow",
     }).append(
-      $("<div>",{
-        class:"menu-item",
+      $("<div>", {
+        class: "menu-item",
       }).append(
-        $("<img>",{
+        $("<img>", {
           src: imageBurger,
           alt: title,
-          class:"menuImg"
+          class: "menuImg"
         }),
-        $("<div>",{
-          class:"menuText"
+        $("<div>", {
+          class: "menuText"
         }).append(
-          $("<h2>",{
-            class:"menuTitle",
+          $("<h2>", {
+            class: "menuTitle",
             text: title
           }),
-          $("<p>",{
+          $("<p>", {
             class: "menuTxt",
-            text:"Quantity: " + quantity
+            text: "Quantity: " + quantity
             // description from object
           }),
-          $("<h2>",{
-            class:"menuPrice",
-            id:"SoftDrinkPrice",
-            text: "$" +burgerPrice*quantity
+          $("<h2>", {
+            class: "menuPrice",
+            id: "SoftDrinkPrice",
+            text: "$" + burgerPrice * quantity
             // price from object
           })
         )
       )
     )
     $(".content-box").append(addImage);
-  }else{
-    return}
+  } else {
+    return
+  }
 });
 
 
 // checkout button to clear local storage and every singe piece of data 
 
-$("#checkoutBtn").on("click",clearAll);
+$("#checkoutBtn").on("click", clearAll);
 
-function clearAll(event){
-  event.preventDefault;var prices0 = parseInt(localStorage.getItem("Cheeseburger"))*products.cheeseburger.price;
-  var prices1 = parseInt(localStorage.getItem("Lot Burger"))*products.price;
-  var prices2 = parseInt(localStorage.getItem("FriedChickenBurger1"))*products.FriedChickenBurger1.price;
-  var prices3 = parseInt(localStorage.getItem("FriedChickenBurger2"))*products.FriedChickenBurger2.price;
-  var prices4 = parseInt(localStorage.getItem("VeganBurger"))*products.VeganBurger.price;
-  var prices5 = parseInt(localStorage.getItem("Chips"))*products.chips.price;
-  var prices6 = parseInt(localStorage.getItem("SoftDrink"))*products.Coke.price;
-    
-    var total = parseInt(prices0)+parseInt(prices2)+parseInt(prices3)+parseInt(prices4)+parseInt(prices5)+parseInt(prices6);
-    console.log(total);
+function getTotalPrice(burgerName, burgerPrice){
+  var saveQuantity = localStorage.getItem(burgerName);
+  if(saveQuantity === undefined){
+    return 0
+  }else{
+    return saveQuantity * burgerPrice
+  }
+}
+
+
+function clearAll(event) {
+  event.preventDefault; 
+  var prices0 = getTotalPrice("Cheeseburger",products.cheeseburger.price);
+  var prices1 = getTotalPrice("Lot Burger",products.cheeseburger.price);
+  var prices2 = getTotalPrice("FriedChickenBurger1",products.cheeseburger.price);
+  var prices3 = getTotalPrice("FriedChickenBurger2",products.cheeseburger.price);
+  var prices4 = getTotalPrice("VeganBurger",products.cheeseburger.price);
+  var prices5 = getTotalPrice("Chips",products.cheeseburger.price);
+  var prices6 = getTotalPrice("SoftDrink",products.cheeseburger.price);
+
+  // if(prices0 || prices1 || prices2 || prices3 || prices4 || prices5 || prices6 == NaN){
+  //   var prices0 ="";
+  //   var prices1 ="";
+  //   var prices2 ="";
+  //   var prices3 ="";
+  //   var prices4 ="";
+  //   var prices5 ="";
+  //   var prices6 ="";
+  // }
+  console.log(prices1);
+  var total = parseInt(prices0) + parseInt(prices1) + parseInt(prices2) + parseInt(prices3) + parseInt(prices4) + parseInt(prices5) + parseInt(prices6);
+  console.log(total);
   // localStorage.clear();
-  var addImage = $("<section>",{
-    class:"menuRow",
+  var addImage = $("<section>", {
+    class: "menuRow",
   }).append(
-    $("<div>",{
-      class:"menuText",
-      text:"Total: $" + total + "   Your Order has been placed!"
+    $("<div>", {
+      class: "menuText",
+      text: "Total: $" + total + "   Your Order has been placed!"
     })
-    )
+  )
 
   $(".content-box").append(addImage);
 
